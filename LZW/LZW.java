2256f80d52b1230b8c05ac60783726a3cc0cdd4f
Ayman17 <77356245+Ayman17@users.noreply.github.com>
2023-11-03 12:47:22 +0200

    edit object name

--- LZW/LZW.java
+++ LZW/LZW.java
@@ -4,8 +4,8 @@ public class LZW {
         String fileName = "TextToCompress";
         String extension = ".txt";
 
-        CompressionLzw lz77 = new CompressionLzw();
-        lz77.compress(fileName + extension);
-        lz77.decompress(fileName + "-compressed" + extension);
+        CompressionLzw lzw = new CompressionLzw();
+        lzw.compress(fileName + extension);
+        lzw.decompress(fileName + "-compressed" + extension);
     }
 }
